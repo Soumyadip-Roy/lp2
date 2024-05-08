99c8103e22850273e5f06ccf42a2654b4fbf6eac
Soumyadip Roy <soumyadiproy1611@gmail.com>
2024-05-09 01:23:04 +0530

    a

--- ASSIGNMENT07/index.js
+++ ASSIGNMENT07/index.js
@@ -1,22 +1,28 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import dotenv from "dotenv"
-import route from "./routes/userRoute.js"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import route from "./routes/userRoute.js";
 
-const app = express ();
+const app = express();
 app.use(bodyParser.json());
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL; 
+const MONGOURL = process.env.MONGO_URL;
 
-mongoose.connect(MONGOURL).then(()=>{
-    console.log("Database connected Successfully.")
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port : ${PORT}`)
-    })
-}).catch(error => console.log(error));
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Database connected Successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
-app.use("/api/user", route)
+app.use("/api/user", route);
+
+// PORT = 4000
+// MONGO_URL = mongodb://0.0.0.0:27017
--- ASSIGNMENT07/routes/userRoute.js
+++ ASSIGNMENT07/routes/userRoute.js
@@ -1,11 +1,16 @@
-import express from "express"
-import { create, deleteUser, fetch, update } from "../controller/userController.js";
+import express from "express";
+import {
+  create,
+  deleteUser,
+  fetch,
+  update,
+} from "../controller/userController.js";
 
 const route = express.Router();
 
-route.get("/getallusers", fetch)
-route.post ("/create",create)
-route.put("/update/:id", update)
-route.delete("/delete/:id",deleteUser)
+route.get("/all", fetch);
+route.post("/create", create);
+route.put("/update/:id", update);
+route.delete("/delete/:id", deleteUser);
 
 export default route;
